e2b5f2945715daa750692abc0520f4dfa490446c
meg-kul5144 <meghaiidbi@gmail.com>
2025-03-13 18:21:50 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+
+# Create flask app
+flask_app = Flask(__name__)
+model = pickle.load(open("C:/Users/61450/Desktop/refonte folder/heart disease prediction/model.pkl", "rb"))
+
+@flask_app.route("/", methods=["GET"])
+def Home():
+    return render_template("index.html")
+
+@flask_app.route("/predict", methods = ["POST"])
+def predict():
+    float_features = [float(x) for x in request.form.values()]
+    features = [np.array(float_features)]
+    prediction = model.predict(features)
+    if(prediction==1):
+        prediction = "Heart disease is likely to prevail"
+    else:
+        prediction = "There are no signs of heart disease"
+    return render_template("index.html", prediction_text = "Heart disease prediction:  {}".format(prediction))
+
+if __name__ == "__main__":
+    flask_app.run(debug=True)
